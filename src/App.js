7fd17258ae0fcc3261bb04d83c9ad2ed75e2e00a
Kakhaber Mchedlishvili <kakha81@gmail.com>
2024-05-04 20:17:21 +0400

    Create HeaderList in Components folder

--- src/App.js
+++ src/App.js
@@ -1,67 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
-import { v4 as uuidv4 } from 'uuid';
+import HeaderList from './components/HeaderList';
 
 function App() {
-  //State hook - "useState"
-  const [newItem, setNewItem] = useState('');
-  const [items, setItems] = useState([]);
-
-  //Helper functions
-
-  function addItem() {
-    if (!newItem) {
-      alert('Enter an item');
-      return;
-    }
-
-    const item = {
-      id: uuidv4(),
-      value: newItem,
-    };
-    setItems((oldList) => [...oldList, item]);
-    setNewItem('');
-  }
-
-  function deleteItem(id) {
-    const newArray = items.filter((item) => item.id !== id);
-    setItems(newArray);
-  }
-
   return (
     <>
-      <div className='App'>
-        {/* 1. Header */}
-        <h1>Todo List App</h1>
-
-        {/* 2. Input (input and button) */}
-        <input
-          type='text'
-          placeholder='Add an item...'
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-        />
-        <button onClick={() => addItem()}>Add</button>
-      </div>
-      <div>
-        {/* 3. List of items (unordered list with list items) */}
-
-        <ul className='listContainer'>
-          {items.map((item) => {
-            return (
-              <li className='listText' key={item.id}>
-                {item.value}
-                <button
-                  className='deleteButton'
-                  onClick={() => deleteItem(item.id)}
-                >
-                  X
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <HeaderList />
     </>
   );
 }
---
+++ src/components/HeaderList.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import '../App.css';
+import { v4 as uuidv4 } from 'uuid';
+
+const HeaderList = () => {
+  //State hook - "useState"
+  const [newItem, setNewItem] = useState('');
+  const [items, setItems] = useState([]);
+
+  //Helper functions
+
+  function addItem() {
+    if (!newItem) {
+      alert('Enter an item');
+      return;
+    }
+
+    const item = {
+      id: uuidv4(),
+      value: newItem,
+    };
+    setItems((oldList) => [...oldList, item]);
+    setNewItem('');
+  }
+
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
+  return (
+    <>
+      <div className='App'>
+        {/* 1. Header */}
+        <h1>ადამიანების სია</h1>
+
+        {/* 2. Input (input and button) */}
+        <input
+          type='text'
+          placeholder='ჩაწერე სახელი და გვარი'
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button onClick={() => addItem()}>დამატება</button>
+      </div>
+      <div>
+        {/* 3. List of items (unordered list with list items) */}
+
+        <ul className='listContainer'>
+          {items.map((item) => {
+            return (
+              <li className='listText' key={item.id}>
+                {item.value}
+                <button
+                  className='deleteButton'
+                  onClick={() => deleteItem(item.id)}
+                >
+                  X
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default HeaderList;
